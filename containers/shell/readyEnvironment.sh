b8ef4708341226a1ca43f43990ada4bcbd7078f4
Erik Andersen <etma@etma.dk>
2025-02-26 22:16:56 +0100

    fix shell script

--- containers/shell/readyEnvironment.sh
+++ containers/shell/readyEnvironment.sh
@@ -8,7 +8,10 @@ git config --global --add safe.directory /workspace
 
 # execute any files named setup*.sh in /root
 # setopt no_nomatch
-for f in /root/setup*.sh; do
-    [ -f "$f" ] && . "$f"
-done
+if ls /root/setup*.sh 1> /dev/null 2>&1; then
+    for f in /root/setup*.sh; do
+        [ -f "$f" ] && . "$f"
+    done
+fi
+
 # setopt nomatch
